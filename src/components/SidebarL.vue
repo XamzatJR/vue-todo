6846e3e92cb81f31a606188adb77806161113938
XamzatJR <bahaev.hamzat95@gmail.com>
2021-11-25 23:24:06 +0300

    worked on the layout

---
+++ src/components/SidebarL.vue
@@ -0,0 +1,65 @@
+<template>
+    <sidebar class="sidebar">
+        <div class="add">
+            <img :src="add" alt="add svg">
+            <div class="add-text">Добавить папку</div>
+        </div>
+        <div v-if="false" class="all-tasks">
+            <img :src="entypo" alt="list svg">
+            <div class="list-item">Все задачи</div>
+        </div>
+    </sidebar>
+</template>
+
+<script>
+import entypo from '@/assets/entypo-list.svg'
+import add from '@/assets/add.svg'
+    export default {
+        setup() {
+            return {
+                entypo,
+                add
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+    background: #f4f6f8;
+    height: 100%;
+    & .all-tasks {
+        display: flex;
+        margin: 57px 0 0 27px;
+        cursor: pointer;
+        & img {
+            width: 18px;
+            height: 18px;
+            margin-right: 6px;
+        }
+    }
+    & .list-item {
+        font-weight: normal;
+        font-size: 14px;
+    }
+}
+.add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 160px;
+    height: 37px;
+    margin: 22px 0 0 20px;
+    cursor: pointer;
+    & img {
+        width: 13px;
+        height: 13px;
+        margin-right: 10px;
+    }
+    &-text {
+        color: #767676;
+        font-weight: 600;
+        font-size: 14px;
+    }
+}
+</style>
