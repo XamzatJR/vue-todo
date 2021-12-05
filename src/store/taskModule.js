61d9d72cec08496785a298110ad69e57fd9a1fc0
XamzatJR <bahaev.hamzat95@gmail.com>
2021-12-05 21:25:16 +0300

    changed the store: removed tasks theyre now in lists, updated mutations and getters

--- src/store/taskModule.js
+++ src/store/taskModule.js
@@ -1,26 +1,41 @@
 export const taskModule = {
     state() {
         return {
-            tasks: [],
             lists: [],
             activeList: null
         }
     },
     getters: {
-
+        filteredTasks(state) {
+            return state.lists.filter(list => {
+                if (state.activeList.id === list.id) {
+                    return list.tasks
+                }
+            })
+        }
     },
     mutations: {
         setTasks(state, task) {
-            state.tasks.push(task);
+            state.lists.forEach(el => {
+                if (el.id === state.activeList.id) {
+                    el.tasks.push(task)  
+                }
+        })
         },
         setLists(state, list) {
             state.lists.push(list);
         },
         setActiveList(state, list) {
             state.activeList = list
+        },
+        removeTask(state, task) {
+            state.lists = state.lists.map(list => {
+                if (list.id === state.activeList.id) {
+                    return {...list, tasks: list.tasks.filter(el => el.id !== task.id)}
+                }
+                return list
+            }
+            )
         }
-    },
-    actions: {
-
     }
 }
