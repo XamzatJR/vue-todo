ad691d5e5cca60888db4a6cb9ebdcb0df38e51c2
XamzatJR <bahaev.hamzat95@gmail.com>
2021-12-07 20:00:55 +0300

    created new page for lists

---
+++ src/pages/ListPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <main class="main">
+        <list :list="activeList" ></list>
+    </main>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import List from '@/components/List.vue';
+    export default {
+        components: {
+            List
+        },
+        computed: {
+            ...mapState({
+                activeList: state => state.task.activeList
+            })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.main {
+    height: 100%;
+    padding-left: 55px;
+}
+</style>
