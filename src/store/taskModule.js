19bdd21cf05ba3461bdbdb19aeccfb64feac31a3
XamzatJR <bahaev.hamzat95@gmail.com>
2021-12-07 23:13:22 +0300

    created localeStorage, updated and added new mutations: removeList and setCompleted.

--- src/store/taskModule.js
+++ src/store/taskModule.js
@@ -1,14 +1,14 @@
 export const taskModule = {
     state() {
         return {
-            lists: [],
+            lists: JSON.parse(localStorage.getItem('lists')) || [],
             activeList: null
         }
     },
     getters: {
         filteredTasks(state) {
             return state.lists.filter(list => {
-                if (state.activeList.id === list.id) {
+                if (state.activeList?.id === list.id) {
                     return list.tasks
                 }
             })
@@ -20,22 +20,38 @@ export const taskModule = {
                 if (el.id === state.activeList.id) {
                     el.tasks.push(task)  
                 }
-        })
+            })
+            localStorage.setItem('lists', JSON.stringify(state.lists))
         },
         setLists(state, list) {
             state.lists.push(list);
+            localStorage.setItem('lists', JSON.stringify(state.lists))
         },
         setActiveList(state, list) {
-            state.activeList = list
+            state.activeList = list;
+        },
+        removeList(state, list) {
+            state.lists = state.lists.filter(el => el.id !== list.id);
+            localStorage.setItem('lists', JSON.stringify(state.lists))
+        },
+        setCompleted(state, task) {
+            state.lists = state.lists.map(list => {
+                if (list.id === task.listId) {
+                    return {...list, tasks: list.tasks.map(el => el.id === task.id ? {...task, completed: !task.completed} : el)}
+                }
+                return list
+            })
+            localStorage.setItem('lists', JSON.stringify(state.lists))
         },
         removeTask(state, task) {
             state.lists = state.lists.map(list => {
-                if (list.id === state.activeList.id) {
+                if (list.id === task.listId) {
                     return {...list, tasks: list.tasks.filter(el => el.id !== task.id)}
                 }
                 return list
-            }
+                }
             )
+            localStorage.setItem('lists', JSON.stringify(state.lists))
         }
     }
 }
