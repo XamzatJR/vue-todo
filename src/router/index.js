a0d43bececf012e0ffc9ac4f822bfdd95f452d81
XamzatJR <bahaev.hamzat95@gmail.com>
2021-12-07 20:02:55 +0300

    added new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from '@/pages/Main.vue';
+import ListPage from '@/pages/ListPage.vue';
 
 const routes = [
   {
     path: '/',
     component: Main,
   },
+  {
+    path: '/list/:id',
+    component: ListPage
+  }
 ];
 
 // eslint-disable-next-line no-unused-vars
